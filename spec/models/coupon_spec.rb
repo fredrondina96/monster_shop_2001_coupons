b547f4ab9f7042f0c66f9f7451c24921f85d1b24
Fred Rondina <fredrondina96@gmail.com>
2020-04-20 17:59:38 -0600

    added test for coupon discount model method

--- spec/models/coupon_spec.rb
+++ spec/models/coupon_spec.rb
@@ -12,4 +12,13 @@ describe Coupon, type: :model do
     it {should belong_to :merchant}
   end
 
+  describe "model methods" do
+    it "discount" do
+      @meg = Merchant.create(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
+      @coupon1 = @meg.coupons.create!(name: "20% off 5 Items", percent_off: 20, quantity_required: 5)
+      
+      expect(@coupon1.discount).to eq(0.8)
+    end
+  end
+
 end
