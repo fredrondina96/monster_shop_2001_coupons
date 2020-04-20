eb97d01c87eb8925ea7b156c07490bbaac024862
Fred Rondina <fredrondina96@gmail.com>
2020-04-20 17:00:33 -0600

    edited subtotal and total methods so they check for, and apply coupons

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -38,20 +38,17 @@ class Cart
   def subtotal(item)
     item.merchant.coupons.each do |coupon|
       if @contents[item.id.to_s] >= coupon.quantity_required
-        return (item.price * @contents[item.id.to_s]) * coupon.discout
+        return (item.price * @contents[item.id.to_s]) * coupon.discount
       end
     end
     item.price * @contents[item.id.to_s]
   end
 
   def total
-    @contents.sum do |item_id,quantity|
-      Item.find(item_id).price * quantity
-    end
+    grand_total = 0.0
+      @contents.keys.each do |item|
+        grand_total += subtotal(Item.find(item))
+      end
+    return grand_total
   end
-
-  def discount_check(item)
-    @contents[item.id.to_s]
-  end
-
 end
