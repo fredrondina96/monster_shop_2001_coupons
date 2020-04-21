903833f16c1cc35f8f5afefe1a37161855c0383f
Fred Rondina <fredrondina96@gmail.com>
2020-04-20 18:00:56 -0600

    added tests for coupons being applied by greatest discount first, and only for correct merchant

--- spec/features/merchant/coupons/coupons_applied_at_checkout_spec.rb
+++ spec/features/merchant/coupons/coupons_applied_at_checkout_spec.rb
@@ -4,10 +4,11 @@ RSpec.describe 'When I view my cart as a user' do
   describe 'Any merchant coupons are automatically applied' do
     before(:each) do
       @meg = Merchant.create(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
+      @mike = Merchant.create(name: "Mike's Print Shop", address: '123 Paper Rd.', city: 'Denver', state: 'CO', zip: 80203)
 
       @tire = @meg.items.create(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 12)
       @paper = @meg.items.create(name: "Lined Paper", description: "Great for writing on!", price: 20, image: "https://cdn.vertex42.com/WordTemplates/images/printable-lined-paper-wide-ruled.png", inventory: 3)
-      @pencil = @meg.items.create(name: "Yellow Pencil", description: "You can write on paper with it!", price: 2, image: "https://images-na.ssl-images-amazon.com/images/I/31BlVr01izL._SX425_.jpg", inventory: 100)
+      @pencil = @mike.items.create(name: "Yellow Pencil", description: "You can write on paper with it!", price: 2, image: "https://images-na.ssl-images-amazon.com/images/I/31BlVr01izL._SX425_.jpg", inventory: 100)
       @coupon1 = @meg.coupons.create!(name: "20% off 5 Items", percent_off: 20, quantity_required: 5)
 
 
@@ -29,7 +30,39 @@ RSpec.describe 'When I view my cart as a user' do
           expect(page).to_not have_content("$500.00")
           expect(page).to have_content("$400.00")
       end
+
+      it "only items from the same merchant as the coupon are discounted" do
+        visit "/items/#{@pencil.id}"
+        click_on "Add To Cart"
+
+        visit "/cart"
+          within "#item-quantity-#{@pencil.id}" do
+            4.times do
+              click_link "+"
+            end
+          end
+        expect(page).to have_content("$10.00")
+      end
+
+      it "if there are two coupons that can be applied, the one with the greater discount is applied" do
+
+        @coupon2 = @meg.coupons.create!(name: "90% off 8 Items", percent_off: 90, quantity_required: 4)
+
+        visit "/items/#{@tire.id}"
+        click_on "Add To Cart"
+
+        visit "/cart"
+          within "#item-quantity-#{@tire.id}" do
+            7.times do
+              click_link "+"
+            end
+          end
+        expect(page).to_not have_content("$640.00")
+        expect(page).to have_content("$80.00")
+      end
+
     end
+
     after(:each) do
       User.destroy_all
       Merchant.destroy_all
