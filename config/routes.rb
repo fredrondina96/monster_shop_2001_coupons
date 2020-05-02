0af90dfb59bcfcc7bfb8db0dff6e14ba8d6ac344
Fred Rondina <fredrondina96@gmail.com>
2020-05-02 14:58:01 -0600

    refactores some routes using resources

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,10 @@ Rails.application.routes.draw do
 
   namespace :admin do
     get "/", to: "dashboard#index"
-    get "/users", to: "users#index"
-    get "/users/:id", to: "users#show"
+    resources :users, only: [:index, :show]
+      # get "/users", to: "users#index"
+      # get "/users/:id", to: "users#show"
+
     get "/merchants", to: "merchants#index"
     get "/merchants/:merchant_id", to: "merchants#show"
     patch "/merchants/:merchant_id", to: "merchants#update"
@@ -42,13 +44,15 @@ Rails.application.routes.draw do
     patch "/itemorders/:id", to: "item_orders#update", as: :item_order_update
   end
 
-  get "/merchants", to: "merchants#index"
-  get "/merchants/new", to: "merchants#new"
-  get "/merchants/:id", to: "merchants#show"
-  post "/merchants", to: "merchants#create"
-  get "/merchants/:id/edit", to: "merchants#edit"
-  patch "/merchants/:id", to: "merchants#update"
-  delete "/merchants/:id", to: "merchants#destroy"
+  resources :merchants
+  # get "/merchants", to: "merchants#index"
+  # get "/merchants/new", to: "merchants#new"
+  # get "/merchants/:id", to: "merchants#show"
+  # post "/merchants", to: "merchants#create"
+  # get "/merchants/:id/edit", to: "merchants#edit"
+  # patch "/merchants/:id", to: "merchants#update"
+  # delete "/merchants/:id", to: "merchants#destroy"
+
   get "/merchants/:merchant_id/items", to: "items#index"
   get "/merchants/:merchant_id/items/new", to: "items#new"
   post "/merchants/:merchant_id/items", to: "items#create"
@@ -70,14 +74,14 @@ Rails.application.routes.draw do
   delete "/cart", to: "cart#empty"
   delete "/cart/:item_id", to: "cart#remove_item"
 
-  get "/orders/new", to: "orders#new"
-  post "/orders", to: "orders#create"
+  resources :orders, only: [:new, :create]
+  # get "/orders/new", to: "orders#new"
+  # post "/orders", to: "orders#create"
   get "/orders/:id", to: "orders#show", as: :order_show
   patch "/orders/:id", to: "orders#cancel", as: :order_cancel
 
 
   get "/register", to: "users#new"
-
 
   post "/profile", to: "users#create"
   get "/profile", to: "users#show"
